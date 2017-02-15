501673ae985cedfa2db99e0027036b8dd3f39e28
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-15 11:13:47 -0600

    restarting game if correct answer is chosen

--- client.js
+++ client.js
@@ -1,6 +1,9 @@
+var colorArray = ['aliceblue', 'azure', 'lightcyan', 'floralwhite', 'mintcream'] //creates a color array
+var randomColor = '';
+
 $(document).ready(function(){ //doc ready start
   console.log('jQuery is ready to go!'); // does my jQuery work?
-  var colorArray = ['aliceblue', 'azure', 'lightcyan', 'floralwhite', 'mintcream'] //creates a color array
+
 
   for (var i = 0; i < colorArray.length; i++) { //for loop goes through array and adds a div based on the arry color
     var newColorBlock = $('<div>'); //creates a new div
@@ -13,22 +16,30 @@ $(document).ready(function(){ //doc ready start
 
   $('#colorBlockContainer').on('click', '.colorBlock', function(){ //looks for clicks on class colorBlock within colorBlockContainer
     //console.log('$(this).data().color is: ', $(this).data().colorOfBlock); //this.data is referencing the div object
+
     var colorOfBlockSelected = $(this).data().colorOfBlock;
     if(colorOfBlockSelected == randomColor){
       $('#successFail').text('YOU GOT IT!');
+      chooseNewRandomColor();
+
     } else {
       $('#successFail').text('OH NO... THAT\S NOT RIGHT');
     }//END IF ELSE
 });//END ON CLICK
 
-  // Select a random color and add it to the dom
-  var randomNumberSelected = randomNumber(0, colorArray.length - 1);
-  var randomColor = colorArray[randomNumberSelected]; // uses randomNumberSelected to find the color at the correct array index
-  //basically the last two lines set a number to a color based on the array index
-  $('#userColorPrompt').text('Your color is: ' + randomColor);
+chooseNewRandomColor();
+
 })//ends doc.ready
 
 
 function randomNumber(min, max){
   return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function chooseNewRandomColor(){
+  // Select a random color and add it to the dom
+  var randomNumberSelected = randomNumber(0, colorArray.length - 1);
+  randomColor = colorArray[randomNumberSelected]; // uses randomNumberSelected to find the color at the correct array index
+  //basically the last two lines set a number to a color based on the array index
+  $('#userColorPrompt').text('Your color is: ' + randomColor);
 }
