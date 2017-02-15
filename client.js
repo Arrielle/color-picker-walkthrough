f494f573075a7bef61db3317cc25bb4304bd3cd3
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-15 10:55:22 -0600

    getting yup nope on color pick to work

--- client.js
+++ client.js
@@ -6,19 +6,25 @@ $(document).ready(function(){ //doc ready start
     var newColorBlock = $('<div>'); //creates a new div
     newColorBlock.css('background-color', colorArray[i]); //adds a color to the empty div
     newColorBlock.addClass('colorBlock'); //adds a class to the empty div
+    newColorBlock.data('colorOfBlock', colorArray[i]);//this adds a key and value to the div on the DOM
     $('#colorBlockContainer').append(newColorBlock); //add div to the dom
+
   } // ends for loop
+
+  $('#colorBlockContainer').on('click', '.colorBlock', function(){ //looks for clicks on class colorBlock within colorBlockContainer
+    //console.log('$(this).data().color is: ', $(this).data().colorOfBlock); //this.data is referencing the div object
+    var colorOfBlockSelected = $(this).data().colorOfBlock;
+    if(colorOfBlockSelected == randomColor){
+      $('#successFail').text('YOU GOT IT!');
+    } else {
+      $('#successFail').text('OH NO... THAT\S NOT RIGHT');
+    }//END IF ELSE
+});//END ON CLICK
 
   // Select a random color and add it to the dom
   var randomNumberSelected = randomNumber(0, colorArray.length - 1);
   var randomColor = colorArray[randomNumberSelected]; // uses randomNumberSelected to find the color at the correct array index
   //basically the last two lines set a number to a color based on the array index
-
-
-
-  console.log(randomNumberSelected);
-  console.log(randomColor);
-
   $('#userColorPrompt').text('Your color is: ' + randomColor);
 })//ends doc.ready
 
