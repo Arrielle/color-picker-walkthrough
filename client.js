6c196bf3cdfdc9cbbdfbdc7372edd7e58607ed5a
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-15 11:55:22 -0600

    add remove blocks

--- client.js
+++ client.js
@@ -1,18 +1,13 @@
-var colorArray = ['aliceblue', 'azure', 'lightcyan', 'floralwhite', 'mintcream'] //creates a color array
+var colorArray = ['aliceblue', 'azure', 'lightcyan', 'floralwhite', 'mintcream', 'cornsilk', 'white', 'gainsboro'] //creates a color array
 var randomColor = '';
+
+var numberOfColors = 4;
+
 
 $(document).ready(function(){ //doc ready start
   console.log('jQuery is ready to go!'); // does my jQuery work?
 
-
-  for (var i = 0; i < colorArray.length; i++) { //for loop goes through array and adds a div based on the arry color
-    var newColorBlock = $('<div>'); //creates a new div
-    newColorBlock.css('background-color', colorArray[i]); //adds a color to the empty div
-    newColorBlock.addClass('colorBlock'); //adds a class to the empty div
-    newColorBlock.data('colorOfBlock', colorArray[i]);//this adds a key and value to the div on the DOM
-    $('#colorBlockContainer').append(newColorBlock); //add div to the dom
-
-  } // ends for loop
+  addColorBlocksToPage();
 
   $('#colorBlockContainer').on('click', '.colorBlock', function(){ //looks for clicks on class colorBlock within colorBlockContainer
     //console.log('$(this).data().color is: ', $(this).data().colorOfBlock); //this.data is referencing the div object
@@ -20,14 +15,25 @@ $(document).ready(function(){ //doc ready start
     var colorOfBlockSelected = $(this).data().colorOfBlock;
     if(colorOfBlockSelected == randomColor){
       $('#successFail').text('YOU GOT IT!');
-      chooseNewRandomColor();
 
     } else {
       $('#successFail').text('OH NO... THAT\S NOT RIGHT');
     }//END IF ELSE
-});//END ON CLICK
+  });//END ON CLICK
 
-chooseNewRandomColor();
+  $('#addColorButton').on('click', function(){
+    if (numberOfColors < colorArray.length){
+    numberOfColors++;
+    $('#colorBlockContainer').empty();
+    addColorBlocksToPage();
+  }
+  });
+
+  $('#removeColorButton').on('click', function(){
+    numberOfColors--;
+    $('#colorBlockContainer').empty();
+    addColorBlocksToPage();
+  });
 
 })//ends doc.ready
 
@@ -38,8 +44,19 @@ function randomNumber(min, max){
 
 function chooseNewRandomColor(){
   // Select a random color and add it to the dom
-  var randomNumberSelected = randomNumber(0, colorArray.length - 1);
+  var randomNumberSelected = randomNumber(0, numberOfColors - 1);
   randomColor = colorArray[randomNumberSelected]; // uses randomNumberSelected to find the color at the correct array index
   //basically the last two lines set a number to a color based on the array index
   $('#userColorPrompt').text('Your color is: ' + randomColor);
 }
+
+function addColorBlocksToPage(){
+  for (var i = 0; i < numberOfColors; i++) { //for loop goes through array and adds a div based on the arry color
+    var newColorBlock = $('<div>'); //creates a new div
+    newColorBlock.css('background-color', colorArray[i]); //adds a color to the empty div
+    newColorBlock.addClass('colorBlock'); //adds a class to the empty div
+    newColorBlock.data('colorOfBlock', colorArray[i]);//this adds a key and value to the div on the DOM
+    $('#colorBlockContainer').append(newColorBlock); //add div to the dom
+    chooseNewRandomColor();
+  }
+}// ends for loop
